0c083cf57c2db4853876e1a3c5b949af3fc87677
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-19 01:12:46 +0700

    Create a ViewHolder for all the sleep data

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -26,26 +26,16 @@ import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * These functions create a formatted string that can be set in a TextView.
  */
 
-/**
- * Returns a string representing the numeric quality rating.
- */
-fun convertNumericQualityToString(quality: Int, resources: Resources): String {
-    var qualityString = resources.getString(R.string.three_ok)
-    when (quality) {
-        -1 -> qualityString = "--"
-        0 -> qualityString = resources.getString(R.string.zero_very_bad)
-        1 -> qualityString = resources.getString(R.string.one_poor)
-        2 -> qualityString = resources.getString(R.string.two_soso)
-        4 -> qualityString = resources.getString(R.string.four_pretty_good)
-        5 -> qualityString = resources.getString(R.string.five_excellent)
-    }
-    return qualityString
-}
+
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
 
 
 /**
@@ -106,4 +96,40 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
     }
 }
 
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
+
+/**
+ * Returns a string representing the numeric quality rating.
+ */
+fun convertNumericQualityToString(quality: Int, resources: Resources): String {
+    var qualityString = resources.getString(R.string.three_ok)
+    when (quality) {
+        -1 -> qualityString = "--"
+        0 -> qualityString = resources.getString(R.string.zero_very_bad)
+        1 -> qualityString = resources.getString(R.string.one_poor)
+        2 -> qualityString = resources.getString(R.string.two_soso)
+        4 -> qualityString = resources.getString(R.string.four_pretty_good)
+        5 -> qualityString = resources.getString(R.string.five_excellent)
+    }
+    return qualityString
+}
+
 class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+
