dcb833a33d5f0a133c72ffc294eae60d5011c691
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-20 23:49:16 +0700

    Get the starter code and inspect the changes to the app

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -41,4 +41,7 @@ interface SleepDatabaseDao{
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId")
     fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId =  :key")
+    fun getNightWithId(key: Long): LiveData<SleepNight>
 }
